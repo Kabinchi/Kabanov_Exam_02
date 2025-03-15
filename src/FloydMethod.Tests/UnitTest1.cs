bf54a978030a0dc1f8c478abaad4211cac9d66b8
Kabinchi <kabinchi@yandex.ru>
2025-03-15 09:47:30 +0300

    Создание проекта для дальнейшего выполнения задачи

---
+++ src/FloydMethod.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FloydMethod.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/FloydMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿private static double[,] Floyd(double[,] a)
+{
+    double[,] d = new double[n, n];
+    d = (double[,])a.Clone();
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= n - 1; j++)
+            for (int k = 0; k <= n - 1; k++)
+                if (d[j, k] > d[j, i - 1] + d[i - 1, k])
+                    d[j, k] = d[j, i - 1] + d[i - 1, k];
+    return d;
+}
